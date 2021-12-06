2924c5f941295f393349237550cb109ca686cb81
juliane1999 <lastjuliane@gmail.com>
2021-12-05 21:57:47 -0800

    modals added to projects

--- portfolio/src/Components/Projects.js
+++ portfolio/src/Components/Projects.js
@@ -1,26 +1,31 @@
 import React,{ useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "antd/dist/antd.css";
 import Carousel from 'react-bootstrap/Carousel'
-import Modal from 'react-bootstrap/Modal'
-import Button from 'react-bootstrap/Button'
+import { Modal, Button } from 'antd';
 
 export default function Projects() {
-    const values = [true];
-    const [fullscreen, setFullscreen] = useState(true);
-    const [show, setShow] = useState(false);
-    
 
-    function handleShow(breakpoint) {
-        setFullscreen(breakpoint);
-        setShow(true);
-      }
+        const [isModalVisible, setIsModalVisible] = useState(false);
+      
+        const showModal = () => {
+          setIsModalVisible(true);
+        };
+      
+        const handleOk = () => {
+          setIsModalVisible(false);
+        };
+      
+        const handleCancel = () => {
+          setIsModalVisible(false);
+        };
     
   
     return (
         <div className = 'project-container'>
 
     <Carousel>
-    <Carousel.Item interval={3000}>
+        <Carousel.Item interval={3000}>
 
         <img
         className="d-block w-100"
@@ -29,48 +34,20 @@ export default function Projects() {
         width='1920'
         height='1080'
         />
+
         <Carousel.Caption>
         <h3>Human Rights First DocDB </h3>
-        </Carousel.Caption>
-        {values.map((v, idx) => (
-            <Button key={idx} className="me-2" onClick={() => handleShow(v)}>
-              Full screen
-              {typeof v === 'string' && `below ${v.split('-')[0]}`}
-            </Button>
-            ))}
-        <Modal show={show} fullscreen={fullscreen} onHide={() => setShow(false)}>
-            <Modal.Header closeButton>
-            <Modal.Title>Frontend & UX/UI Contribution</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                Modal body content
-            </Modal.Body>
+        <>
+        <Button type="primary" onClick={showModal}>
+            More Info
+        </Button>
+        <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
         </Modal>
-        </Carousel.Item>
-
-        <Carousel.Item interval={3000}>
-
-            <img
-            className="d-block w-100"
-            src="https://images.unsplash.com/photo-1547534887-8d299f2c126b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDg2fDZzTVZqVExTa2VRfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-            alt="Second slide"/>
-
-            <Carousel.Caption>
-            <h3>African Marketplace</h3>
-            {values.map((v, idx) => (
-            <Button key={idx} className="me-2" onClick={() => handleShow(v)}>
-              Full screen
-              {typeof v === 'string' && `below ${v.split('-')[0]}`}
-            </Button>
-            ))}
-            <Modal show={show} fullscreen={fullscreen} onHide={() => setShow(false)}>
-            <Modal.Header closeButton>
-            <Modal.Title>Christine</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>Second modal</Modal.Body>
-            </Modal>    
-            </Carousel.Caption>
-
+    </>
+        </Carousel.Caption>
         </Carousel.Item>
 
         <Carousel.Item interval={3000}>
@@ -81,18 +58,38 @@ export default function Projects() {
             />
             <Carousel.Caption>
             <h3>E-Comm</h3>
-            {values.map((v, idx) => (
-            <Button key={idx} className="me-2" onClick={() => handleShow(v)}>
-              Full screen
-              {typeof v === 'string' && `below ${v.split('-')[0]}`}
+    <>
+        <Button type="primary" onClick={showModal}>
+            More Info
+        </Button>
+        <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+        </Modal>
+    </>
+            </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item interval={3000}>
+
+            <img
+            className="d-block w-100"
+            src="https://images.unsplash.com/photo-1547534887-8d299f2c126b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDg2fDZzTVZqVExTa2VRfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            alt="Second slide"/>
+
+            <Carousel.Caption>
+            <h3>African Marketplace</h3>
+            <>
+            <Button type="primary" onClick={showModal}>
+            More Info
             </Button>
-            ))}
-            <Modal show={show} fullscreen={fullscreen} onHide={() => setShow(false)}>
-            <Modal.Header closeButton>
-            <Modal.Title>POtato</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>third modal</Modal.Body>
+            <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
             </Modal>
+            </>
             </Carousel.Caption>
             </Carousel.Item>
 
@@ -104,18 +101,16 @@ export default function Projects() {
 
             <Carousel.Caption>
             <h3>Portfolio</h3>
-            {values.map((v, idx) => (
-            <Button key={idx} className="me-2" onClick={() => handleShow(v)}>
-              Full screen
-              {typeof v === 'string' && `below ${v.split('-')[0]}`}
+            <>
+            <Button type="primary" onClick={showModal}>
+                More Info
             </Button>
-            ))}
-            <Modal show={show} fullscreen={fullscreen} onHide={() => setShow(false)}>
-            <Modal.Header closeButton>
-            <Modal.Title>Hello</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>fourth modal</Modal.Body>
+            <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
             </Modal>
+            </>
             </Carousel.Caption>
             </Carousel.Item>
 
@@ -124,7 +119,6 @@ export default function Projects() {
       </div>
     )
     }
-// }
 
 
 
