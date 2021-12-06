dc5a91294e84a332961a5750099ea5b475859f8c
juliane1999 <lastjuliane@gmail.com>
2021-12-06 00:54:12 -0800

    cards added to about page

--- portfolio/src/Components/About.js
+++ portfolio/src/Components/About.js
@@ -1,24 +1,42 @@
+/* eslint-disable react/jsx-no-duplicate-props */
 import React from 'react'
-import tree from '../Assets/About.png'
-import Cards from './Card'
-import Card from 'react-bootstrap/Card'
+// import tree from '../Assets/About.png'
+import { Card } from "react-bootstrap";
+import './Box.css'
 
 export default function About() {
 
+    const cardInfo = [
+        {
+          image: "https://i.insider.com/50f967f56bb3f7830a000019",
+          title: "HTML5",
+          text: "",
+        },
+        {
+          image:
+            "https://cdn.vox-cdn.com/thumbor/M1qLla2h-V_2yV_Z4nF_NHH_tjA=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/18286450/usa_today_12495932.jpg",
+          title: "CSS",
+          text: "",
+        },
+      ];
+   
+        
+  const renderCard = (card, index) => {
     return (
         <div className = 'about-img'>
-            <img src = {tree} alt = 'tree'/>
-            <Cards>
-                <Card.Body>
-                <Card.Title>HTML5</Card.Title>
-                <Card.Text>
-                    HI
-                </Card.Text>
-                </Card.Body>
-            </Cards>
-            <Cards>
-            </Cards>
+        {/* <img src = {tree} alt = 'tree'/> */}
+        <Card style={{ width: "18rem" }} key={index} className="box">
+        <Card.Img variant="top" src="holder.js/100px180" src={card.image} />
+        <Card.Body>
+          <Card.Title>{card.title}</Card.Title>
+          <Card.Text>{card.text}</Card.Text>
+        </Card.Body>
+      </Card>
+      </div>
+    );
+  };
 
-        </div>
-    )
-}
+  return <div className="grid">{cardInfo.map(renderCard)}</div>;
+};
+   
+
