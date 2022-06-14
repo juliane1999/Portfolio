b58d923c7e163aa14f097a565b657de9ac8aea32
juliane1999 <lastjuliane@gmail.com>
2022-06-14 01:55:40 -0700

    added modals

--- portfolio/src/Components/Projects.js
+++ portfolio/src/Components/Projects.js
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react/jsx-no-target-blank */
-import React,{ useContext } from 'react'
-import { Link } from 'react-router-dom'
+import React,{ useState,useContext } from 'react'
+import { Modal, Button, Image } from 'antd';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "antd/dist/antd.css";
 import ThemeContext from '../Themes/ThemeContext'
@@ -10,6 +10,8 @@ import ecomm from '../Assets/Ecomm.png'
 import market from '../Assets/Market.png'
 import port from '../Assets/Port.png'
 import Pipe from '../Components/Pipe'
+import { ProjectsData } from '../Data/Frontend'
+import { DesignData } from '../Data/Design';
 import '../Style/Projects.css'
 
 
@@ -17,108 +19,208 @@ const Project = () => {
 
   const theme = useContext(ThemeContext)
 
-  return (
-    <div className='projects' style={theme}>
+  const [isModalVisible, setIsModalVisible] = useState(false)
+  const [modalData, setModalData] = useState([])
 
-      <div className='main-title'>
-        <h1 id='front-end' style={theme}>Front-end</h1>
+  const showModal = () => {
+      setIsModalVisible(true)
+  }
+  const handleOk = () => {
+      setIsModalVisible(false)
+  }
+  const handleCancel = () => {
+      setIsModalVisible(false)
+  }
+
+return (
+  
+  <>
+
+  <div className='main-title' style={theme}>
+
+    <h1 id='front-end' style={theme} >Front-end</h1>
+
+      <div className = 'container' style={theme}>
+
+        {ProjectsData.map(project => ( 
+
+          <div className='projects' style={theme}>
+
+          <div className='frontend'>
+              <p style={theme} className='title'>{project.title}</p>
+                {/* <img className='bg-images' src={project.image} alt='hrf'/> */}
+                  <div className='hashtags'>
+                    <p style={theme}>{project.hashtags}</p>
+                  </div>
+
+                  <Button type="primary" onClick={() => {
+                    setModalData(project)
+                    showModal()
+                    }}>
+                    More Details
+                  </Button>
+
+                  <Modal title={modalData.title} visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                      <p>Description: {modalData.description}</p>
+                      <p>Role: {modalData.role}</p>
+                      <p>Contribution: {modalData.duties}</p>
+                      <p><a target="_blank" rel="noopener noreferrer" href={modalData.github}>Visit Site</a></p>
+                  </Modal>
+          </div>
+          </div>
+          ))}
       </div>
+  
+    <Pipe />
 
-      <div className='hrf'>
-        <a target='_blank' href = 'https://github.com/juliane1999/human-rights-first-docdb-fe'>
-          <p style={theme} className='title'>Human Rights First</p>
-            <img className='bg-images' src={HRF} alt='hrf'/>
-              <div className='hashtags'>
-                <p style={theme}>#React #JavaScript #CSS #XML #AntDesign #DocSearchTool </p>
-              </div>
-        </a>
-      </div>
+    <span id='empty'></span>
 
-      <div className='market'>
-        <a target='_blank' href = 'https://african-marketplace-project.vercel.app/'>
-          <p style={theme} className='title'>African Marketplace</p>
-            <img className='bg-images' src={market} alt='market'/>
-              <div className='hashtags'>
-                <p style={theme}>#React #JavaScript #Redux #CSS #XML #Node.js #Express #Vercel</p>
-              </div>
-        </a>
-      </div>
+  <h1 id='design' style={theme}>UX/UI</h1>
 
-      <div className='ecomm'>
-        <a target='_blank' href = 'https://github.com/c-and-j/front-end'>
-          <p style={theme} className='title'>Ecomm</p>
-            <img className='bg-images' src={ecomm} alt='ecomm'/>
-              <div className='hashtags'>
-                <p style={theme}>#React #JavaScript #Redux # XML #Tailwind #API #Heroku</p>
-              </div>
-        </a>
-      </div>
+  <div className = 'container2' style={theme}>
 
-      <div className='portfolio'>
-        <p className='title'>Portfolio</p>
-          <img className='bg-images' src={port} alt='portfolio'/>
-            <div className='hashtags'>
-              <p style={theme}>#React #JavaScript #SCSS #ContextAPI #CreateGlobalStyle #PixelArt</p>
-            </div>
-      </div>
+{DesignData.map(project => ( 
 
-      <Pipe />
+  <div className='projects' style={theme}>
 
-      <span id='empty'></span>
+  <div className='frontend'>
+      <p style={theme} className='title'>{project.title}</p>
+        {/* <img className='bg-images' src={project.image} alt='hrf'/> */}
+          <div className='hashtags'>
+            <p style={theme}>{project.hashtags}</p>
+          </div>
 
-      <div className='main-title'>
-        <h1 id='design' style={theme}>UX/UI</h1>
-      </div>
+          <Button type="primary" onClick={() => {
+            setModalData(project)
+            showModal()
+            }}>
+            More Details
+          </Button>
 
-      <div className='figma'>
-        <a>
-        <Link to = '/figma' style={{textDecoration: 'none'}}/>
-          <p style={theme} className='title'>Human Rights First</p>
-            <img className='bg-images' src={HRF} alt='hrf'/>
-              <div className='hashtags'>
-                <p id='fig-hash' style={theme}>#Figma #Whimsical #Mockup #Wireframes</p>
-              </div>
-        </a>
-      </div>
+          <Modal title={modalData.title} visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+              <p>Description: {modalData.description}</p>
+              <p>Contribution: {modalData.duties}</p>
+              <p><a target="_blank" rel="noopener noreferrer" href={modalData.github}>Visit Site</a></p>
+          </Modal>
+  </div>
+  </div>
+  ))}
+</div>
 
-      <div className='figma'>
-        <a>
-        <Link to = '/figma' style={{textDecoration: 'none'}}/>
-          <p style={theme} className='title'>Coming Soon</p>
-            <img className='bg-images' src={market} alt='market'/>
-              <div className='hashtags'>
-                <p style={theme}></p>
-              </div>
-        </a>
-      </div>
+  </div>
 
-      <div className='figma'>
-        <a>
-        <Link to = '/figma' style={{textDecoration: 'none'}}/>
-          <p className='title'>Ecomm</p>
-            <img className='bg-images' src={ecomm} alt='ecomm'/>
-              <div className='hashtags'>
-              <p id='fig-hash' style={theme}>#Sketch #Invision #Mockup #Wireframes</p>
-              </div>
-        </a>
-      </div>
+  
 
-      <div className='figma'>
-        <a>
-        <Link to = '/figma' style={{textDecoration: 'none'}}/>
-          <p className='title'>Portfolio</p>
-            <img className='bg-images' src={port} alt='portfolio'/>
-              <div className='hashtags'>
-              <p id='fig-hash' style={theme}>#Figma #PhotoShop #Krita #ClipStudioPaint #Mockup #Wireframes</p>
-              </div>
-        </a>
-      </div>
 
-    </div>
+</>
+
+ )
+}
+
+
+
+//   return (
+//     <div className='projects' style={theme}>
+
+//       <div className='main-title'>
+//         <h1 id='front-end' style={theme}>Front-end</h1>
+//       </div>
+
+//       <div className='hrf'>
+//         <a target='_blank' href = 'https://github.com/juliane1999/human-rights-first-docdb-fe'>
+//           <p style={theme} className='title'>Human Rights First</p>
+//             <img className='bg-images' src={HRF} alt='hrf'/>
+//               <div className='hashtags'>
+//                 <p style={theme}>#React #JavaScript #CSS #XML #AntDesign #DocSearchTool </p>
+//               </div>
+//         </a>
+//       </div>
+
+//       <div className='market'>
+//         <a target='_blank' href = 'https://african-marketplace-project.vercel.app/'>
+//           <p style={theme} className='title'>African Marketplace</p>
+//             <img className='bg-images' src={market} alt='market'/>
+//               <div className='hashtags'>
+//                 <p style={theme}>#React #JavaScript #Redux #CSS #XML #Node.js #Express #Vercel</p>
+//               </div>
+//         </a>
+//       </div>
+
+//       <div className='ecomm'>
+//         <a target='_blank' href = 'https://github.com/c-and-j/front-end'>
+//           <p style={theme} className='title'>Ecomm</p>
+//             <img className='bg-images' src={ecomm} alt='ecomm'/>
+//               <div className='hashtags'>
+//                 <p style={theme}>#React #JavaScript #Redux # XML #Tailwind #API #Heroku</p>
+//               </div>
+//         </a>
+//       </div>
+
+//       <div className='portfolio'>
+//         <p className='title'>Portfolio</p>
+//           <img className='bg-images' src={port} alt='portfolio'/>
+//             <div className='hashtags'>
+//               <p style={theme}>#React #JavaScript #SCSS #ContextAPI #CreateGlobalStyle #PixelArt</p>
+//             </div>
+//       </div>
+
+//       <Pipe />
+
+//       <span id='empty'></span>
+
+//       <div className='main-title'>
+//         <h1 id='design' style={theme}>UX/UI</h1>
+//       </div>
+
+//       <div className='figma'>
+//         <a>
+//         <Link to = '/figma' style={{textDecoration: 'none'}}/>
+//           <p style={theme} className='title'>Human Rights First</p>
+//             <img className='bg-images' src={HRF} alt='hrf'/>
+//               <div className='hashtags'>
+//                 <p id='fig-hash' style={theme}>#Figma #Whimsical #Mockup #Wireframes</p>
+//               </div>
+//         </a>
+//       </div>
+
+//       <div className='figma'>
+//         <a>
+//         <Link to = '/figma' style={{textDecoration: 'none'}}/>
+//           <p style={theme} className='title'>Coming Soon</p>
+//             <img className='bg-images' src={market} alt='market'/>
+//               <div className='hashtags'>
+//                 <p style={theme}></p>
+//               </div>
+//         </a>
+//       </div>
+
+//       <div className='figma'>
+//         <a>
+//         <Link to = '/figma' style={{textDecoration: 'none'}}/>
+//           <p className='title'>Ecomm</p>
+//             <img className='bg-images' src={ecomm} alt='ecomm'/>
+//               <div className='hashtags'>
+//               <p id='fig-hash' style={theme}>#Sketch #Invision #Mockup #Wireframes</p>
+//               </div>
+//         </a>
+//       </div>
+
+//       <div className='figma'>
+//         <a>
+//         <Link to = '/figma' style={{textDecoration: 'none'}}/>
+//           <p className='title'>Portfolio</p>
+//             <img className='bg-images' src={port} alt='portfolio'/>
+//               <div className='hashtags'>
+//               <p id='fig-hash' style={theme}>#Figma #PhotoShop #Krita #ClipStudioPaint #Mockup #Wireframes</p>
+//               </div>
+//         </a>
+//       </div>
+
+//     </div>
 
     
-  )
-}
+//   )
+// }
 
 
 export default Project
